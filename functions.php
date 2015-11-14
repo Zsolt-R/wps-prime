07165fedcf1b621a4c7bdfa735b0d49767a477ae
Zsolt Revay <zsolt.revay.g@gmail.com>
2015-11-14 10:39:12 +0200

    Update Picturefill from 2..1.0 to 3.0.1

--- functions.php
+++ functions.php
@@ -221,7 +221,7 @@ function wps_prime_scripts() {
 	wp_enqueue_script( 'site_js', get_template_directory_uri() . '/js/min/site.min.js', array('swiper_core'), '20141029', true );
 
 	//PICTUREFILL defer load 
-	wp_enqueue_script( 'picturefill_core', get_template_directory_uri() . '/js/min/picturefill.min.js#deferload', array(), '2.1.0', true );
+	wp_enqueue_script( 'picturefill_core', get_template_directory_uri() . '/js/min/picturefill.min.js#deferload', array(), '3.0.1', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
