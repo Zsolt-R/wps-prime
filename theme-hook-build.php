458fcf85c65df0fc9d39fb53115352327b4be9db
Zsolt Revay <zsolt.revay.g@gmail.com>
2016-01-31 16:00:43 +0200

    Cleanup unused setting

--- theme-hook-build.php
+++ theme-hook-build.php
@@ -49,37 +49,6 @@ add_action( 'layout_header__img', 'theme_site_logo',10 );
  */
 add_action( 'layout_header__body', 'theme_site_navigation',10 );
 
-/**
- * Add filter to remove the site-menu
- * filter must be executed before the nav hooks to template
- * Filter Hooked to body_start just before the layout_header__body fires
- *
- * Remove action if page template is 'templates/template-cleanpage.php'
- */
-add_filter( 'body_start','conditional_remove_site_nav' );
-
-/**
- * Conditional remove sitenav function
- */
-function conditional_remove_site_nav() {
-
-	global $post;
-
-	// Conditional check!
-	if ( ! is_404() && ( null !== $post) ) {
-
-		// Get current template!
-		$template = get_page_template_slug( $post->ID );
-
-		// Check for template!
-		if ( 'templates/template-cleanpage.php' === $template ) {
-
-			remove_action( 'layout_header__body','theme_site_navigation',1 );
-			remove_action( 'theme_header','theme_site_navigation',99 );
-			remove_action( 'theme_header','theme_site_navigation',0 );
-		}
-	}
-}
 
 /**
  *  Footer Parts
