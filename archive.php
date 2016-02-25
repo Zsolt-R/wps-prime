35fa5464ad1e2ad6aa9be3b363bcede5661f9764
Zsolt Revay <zsolt.revay.g@gmail.com>
2016-02-25 14:54:54 +0200

    Fix correct title tag wrap

--- archive.php
+++ archive.php
@@ -17,7 +17,6 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
             <header class="page-header">
-                <h1 class="page-title">
                 <?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
