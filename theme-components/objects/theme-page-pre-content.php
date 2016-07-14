2adf47ba055c26092f5a536e710a95ca3c3d4109
Zsolt Revay <zsolt.revay.g@gmail.com>
2016-07-14 20:03:47 +0300

    Make theme pre content pluggable

--- theme-components/objects/theme-page-pre-content.php
+++ theme-components/objects/theme-page-pre-content.php
@@ -4,12 +4,15 @@
  *
  * @package wps_prime
  */
- 
-function theme_page_pre_content(){
-  if(is_page()){
-    $content = get_post_meta(get_the_ID(),'page_pre_content',true);
-    if('' !== $content){
-       echo '<section class="site-pre-content">'. do_shortcode( $content ) .'</section>';
-     }
-   }
+if(!function_exists('theme_page_pre_content')){
+
+	function theme_page_pre_content(){
+	  if(is_page()){
+	    $content = get_post_meta(get_the_ID(),'page_pre_content',true);
+	    if('' !== $content){
+	       echo '<section class="site-pre-content">'. do_shortcode( $content ) .'</section>';
+	     }
+	   }
+	}
+	
 }
