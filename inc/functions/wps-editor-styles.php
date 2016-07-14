ee588b5361c5f21a49bffd0c6dcf0cbf407a7777
Zsolt Revay <zsolt.revay.g@gmail.com>
2016-07-14 19:55:00 +0300

    Update editor style includer

--- inc/functions/wps-editor-styles.php
+++ inc/functions/wps-editor-styles.php
@@ -11,18 +11,24 @@
  */
 function wps_editor_styles() {
 
-	$style = false;
+	$style_exist = false;
+	$style = '';
 
 	// Check if style is avaliable in child theme or parent theme.
 	if ( file_exists( get_stylesheet_directory().'/editor-style.css' ) ) {
+
+		$style_exist = true;
 
 		$style = get_stylesheet_directory_uri().'/editor-style.css';
 
 	} elseif ( file_exists( get_template_directory().'/editor-style.css' ) ) {
 
+		$style_exist = true;
+
 		$style = get_template_directory_uri().'/editor-style.css';
 	}
-	if ( $style ) { add_editor_style( $style ); }
+	if ( $style_exist ) { add_editor_style( $style ); 
+	}
 }
 add_action( 'init','wps_editor_styles' );
 
