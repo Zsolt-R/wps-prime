1bc1a7d071ea8f0f22c86f0a82ad37beee077ec4
Zsolt Revay <zsolt.revay.g@gmail.com>
2017-01-23 14:20:02 +0200

    Add media id to image gallery admin column

---
+++ inc/admin/wps-admin-column-show-media-id.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Media Library media ID admin column
+ *
+ * @package wps_prime
+ */
+
+function column_id($columns) {
+    $columns['colID'] = __('ID');
+    return $columns;
+}
+add_filter( 'manage_media_columns', 'column_id' );
+function column_id_row($columnName, $columnID){
+    if($columnName == 'colID'){
+       echo $columnID;
+    }
+}
+add_filter( 'manage_media_custom_column', 'column_id_row', 10, 2 );
