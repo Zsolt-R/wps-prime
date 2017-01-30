782cef4ff9521474188987af46d40662ac91c248
Zsolt Revay <zsolt.revay.g@gmail.com>
2017-01-30 11:58:07 +0200

    Add extra navigation container class (aid css styling)

--- template-components/components/theme-site-nav.php
+++ template-components/components/theme-site-nav.php
@@ -16,7 +16,7 @@ if ( ! function_exists( 'main_site_nav' ) ) {
 
 		echo '<nav id="site-nav"'. main_nav_class() .' role="navigation" data-ui-component="site-main-nav">';
 
-		wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'site-nav__list', 'walker' => new Theme_Menu_Object() ) );
+		wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'site-nav__list', 'container_class'=>'site-nav__menu-container', 'walker' => new Theme_Menu_Object() ) );
 		echo '</nav><!-- #site-nav -->';
 
 	}
@@ -31,7 +31,7 @@ if ( ! function_exists( 'main_site_nav_mobile' ) ) {
 
 		echo '<nav id="site-mobile-nav"'. main_mobile_nav_class() .' role="navigation-mobile" data-ui-component="site-main-nav-mobile">';
 
-		wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'site-mobile-nav__list', 'walker' => new Theme_Menu_Object() ) );
+		wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'site-mobile-nav__list', 'container_class'=>'site-nav-mobile__menu-container', 'walker' => new Theme_Menu_Object() ) );
 		echo '</nav><!-- #site-mobile-nav -->';
 
 	}
