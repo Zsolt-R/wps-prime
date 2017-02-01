eea8bb7435b18f225161fd6d21fb822da7e97092
Zsolt Revay <zsolt.revay.g@gmail.com>
2017-02-01 16:06:55 +0200

    Remove contact form 7 script deregister

--- inc/integrations/cf7-finetune.php
+++ inc/integrations/cf7-finetune.php
@@ -6,47 +6,42 @@
  */
 
 /**
- * Disable scripts
- */
-function disable_contact7_scripts() {
-	wp_deregister_script( 'contact-form-7' );
-}
-
-
-/**
  * Disable styles
  */
-function disable_contact7_styles() {
-	wp_deregister_style( 'contact-form-7' );
-	wp_deregister_style( 'contact-form-7-rtl' );
+function disable_contact7_styles() 
+{
+    wp_dequeue_style('contact-form-7');
+    wp_dequeue_style('contact-form-7-rtl');
 }
 
 /**
  * Change preloader image
  */
-function my_wpcf7_ajax_loader() {
-	return  WPS_ASSETS_URI .'images/ajax-loader.gif';
+function wps_custom_wpcf7_ajax_loader() 
+{
+    return  WPS_IMAGE_DIR_URI .'/ajax-loader.gif';    
 }
 
 /**
  * Allow other shortcodes in Contact Form 7
  *
- * @param obj $form Contact form object.
+ * @param  obj $form Contact form object.
  * @return obj
  */
-function wps_wpcf7_form_elements( $form ) {
-	$form = do_shortcode( $form );
-	return $form;
+function wps_wpcf7_form_elements( $form ) 
+{
+    $form = do_shortcode($form);
+    return $form;
 }
 
 /**
  * If Contact Form 7 plugin Exists, do the tweaks
  */
-if ( function_exists( 'wpcf7_contact_form' ) ) {
+if (function_exists('wpcf7_contact_form') ) {
 
-	// Deregister Contact From 7 default Styles.
-	add_action( 'wp_enqueue_scripts', 'disable_contact7_styles' );
-	add_filter( 'wpcf7_ajax_loader', 'my_wpcf7_ajax_loader' );
-	add_filter( 'wpcf7_form_elements', 'wps_wpcf7_form_elements' );
+    // Deregister Contact From 7 default Styles.
+    add_action('wp_enqueue_scripts', 'disable_contact7_styles');
+    add_filter('wpcf7_ajax_loader', 'wps_custom_wpcf7_ajax_loader');
+    add_filter('wpcf7_form_elements', 'wps_wpcf7_form_elements');
 
 }
