d2d8de144cde15327840908b11a203bec26412f8
Zsolt Revay <zsolt.revay.g@gmail.com>
2017-02-08 20:32:11 +0200

    Prefix and make site logo function pluggable

--- template-components/components/theme-site-logo.php
+++ template-components/components/theme-site-logo.php
@@ -10,7 +10,9 @@
 /**
  * Website logo function
  */
-function theme_site_logo() {
+
+if ( ! function_exists( 'wps_theme_site_logo' ) ) {
+function wps_theme_site_logo() {
 
 	$output = '';
 	$logo_img = wps_get_theme_option( 'company_logo' );
@@ -22,26 +24,27 @@ function theme_site_logo() {
 
 	if ( !$logo_img ) {
 
-		if ( is_front_page() && is_home() ){
+			if ( is_front_page() && is_home() ){
 			$html_tag = 'h1';
-		}else{
-			$html_tag = 'p';
+			}else{
+				$html_tag = 'p';
+			}
+
+			$output .= '<'.$html_tag.' class="site-title"><a href="'. esc_url( home_url( '/' ) ) .'" rel="home">'. get_bloginfo( 'name' ) .'</a></'.$html_tag.'>';
+
+			$description = get_bloginfo( 'description', 'display' );
+			if ( $description || is_customize_preview() ){
+					$output .= '<p class="site-description">'. $description.'</p>'; /* WPCS: xss ok. */
+			}
+
+		} else {	
+				$output .= '<a title="'. get_bloginfo( 'name' ) .'" href="'. esc_url( home_url( '/' ) )  .'">';
+				$output .= '<img src="'. $logo_img .'" alt="'. get_bloginfo( 'name' ) .'" class="brand-logo"/>';
+				$output .= '</a>';
 		}
 
-		$output .= '<'.$html_tag.' class="site-title"><a href="'. esc_url( home_url( '/' ) ) .'" rel="home">'. get_bloginfo( 'name' ) .'</a></'.$html_tag.'>';
+		$output .= '</div><!-- .site-branding -->';
 
-		$description = get_bloginfo( 'description', 'display' );
-		if ( $description || is_customize_preview() ){
-				$output .= '<p class="site-description">'. $description.'</p>'; /* WPCS: xss ok. */
-		}
-
-	} else {	
-			$output .= '<a title="'. get_bloginfo( 'name' ) .'" href="'. esc_url( home_url( '/' ) )  .'">';
-			$output .= '<img src="'. $logo_img .'" alt="'. get_bloginfo( 'name' ) .'" class="brand-logo"/>';
-			$output .= '</a>';
+		echo $output;/* WPCS: xss ok. */
 	}
-
-	$output .= '</div><!-- .site-branding -->';
-
-	echo $output;/* WPCS: xss ok. */
 }
