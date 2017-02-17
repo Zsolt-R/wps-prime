128f63196dcbb7e7de37163921f3332769d99d6a
Zsolt Revay <zsolt.revay.g@gmail.com>
2017-02-17 12:20:00 +0200

    Create mobile menu walker (fix duplicate id bug)

--- template-components/components/theme-site-nav.php
+++ template-components/components/theme-site-nav.php
@@ -29,9 +29,9 @@ if ( ! function_exists( 'wps_main_site_nav_mobile' ) ) {
 	 */
 	function wps_main_site_nav_mobile() {
 
-		echo '<nav id="site-mobile-nav"'. wps_site_mobile_nav_class() .' role="navigation-mobile" data-ui-component="site-main-nav-mobile">';
+		echo '<nav id="site-mobile-nav"'. wps_site_mobile_nav_class() .' data-ui-component="site-main-nav-mobile">';
 
-		wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'site-mobile-nav__list', 'container_class'=>'site-nav-mobile__menu-container', 'walker' => new Theme_Menu_Object() ) );
+		wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'site-mobile-nav__list', 'container_class'=>'site-nav-mobile__menu-container', 'walker' => new Theme_Mobile_Menu_Object() ) );
 		echo '</nav><!-- #site-mobile-nav -->';
 
 	}
@@ -41,7 +41,7 @@ if ( ! function_exists( 'wps_main_site_mobile_nav_toggler' ) ) {
 
 function wps_main_site_mobile_nav_toggler() {
 
-		$ico = '<div class="site-mobile-nav-icon"><span></span><span></span><span></span><span></span></div>';
+		$ico = '<span class="site-mobile-nav-icon"><span></span><span></span><span></span><span></span></span>';
 
 		echo '<button class="site-mobile-nav-toggler"  id="nav-toggler" data-ui-component="menu-toggle-button">';
 		echo $ico;
